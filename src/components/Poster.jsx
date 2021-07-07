327fae3e34ca4747f1c5503da0d865a760cb55ff
Evgeniy Om <evgenicuss@gmail.com>
2021-07-07 23:29:05 +0300

    Добавлен постер

---
+++ src/components/Poster.jsx
@@ -0,0 +1,8 @@
+function Poster() {
+   return (
+      <img src="https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/d55c3446-6313-4d17-a2f9-6b035dd5f698/360"
+           alt="111" width="auto" height="350px"/>
+   )
+}
+
+export default Poster
