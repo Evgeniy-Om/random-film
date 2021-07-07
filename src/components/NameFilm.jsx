08fce96552a000688fdfcd7a74d04dd3d4da8f33
Evgeniy Om <evgenicuss@gmail.com>
2021-07-07 23:29:26 +0300

    Добавлено название фильма

---
+++ src/components/NameFilm.jsx
@@ -0,0 +1,14 @@
+import {css} from "@emotion/css"
+
+function NameFilm() {
+   return (
+      <p className={css`
+        font-size: 25px;
+        margin: 20px 0 30px;
+      `}>
+         Терминал
+      </p>
+   )
+}
+
+export default NameFilm
