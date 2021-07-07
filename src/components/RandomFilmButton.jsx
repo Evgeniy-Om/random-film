1d56a3b5de79f275e5af1f3849876b16bc990a85
Evgeniy Om <evgenicuss@gmail.com>
2021-07-07 23:29:42 +0300

    Добавлено кнопка "Случайный фильм"

---
+++ src/components/RandomFilmButton.jsx
@@ -0,0 +1,24 @@
+import {css} from "@emotion/css"
+import {Button} from "@material-ui/core"
+
+function generateRandomFilm() {
+   console.log(222);
+}
+
+function RandomFilmButton() {
+   return (
+      <Button
+         onClick={generateRandomFilm}
+         variant="contained"
+         color="primary"
+         className={css`
+                    &.MuiButton-root {
+                      padding: 10px 50px;
+                    }
+                  `}>
+         Случайный фильм
+      </Button>
+   )
+}
+
+export default RandomFilmButton
