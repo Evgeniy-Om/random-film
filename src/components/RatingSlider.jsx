61542aaacf0a117ec04254a1b9fdad962e6734ec
Evgeniy Om <evgenicuss@gmail.com>
2021-07-13 23:57:23 +0300

    В Слайдерах "Год" и "Рейтинг" заменён useState на Redux Toolkit

--- src/components/RatingSlider.jsx
+++ src/components/RatingSlider.jsx
@@ -1,20 +1,35 @@
-import MySlider from "./MySlider"
-
-const marks = [
-   {
-      value: 0,
-      label: "0"
-   },
-   {
-      value: 10,
-      label: "10"
-   }
-];
+import {useDispatch, useSelector} from "react-redux";
+import {selectRating} from "../toolkitRedux/toolkitSlice";
+import {Box, Slider, Typography} from "@material-ui/core";
 
 function RatingSlider() {
+   const {selectedRating, marks} = useSelector(state => {
+      /**
+       * @param {{toolkit:string}} state
+       */
+      return state.toolkit.rating;
+   })
+   const dispatch = useDispatch()
+
+   const handleChange = (event, newValue) => {
+      dispatch(selectRating(newValue))
+   }
+
    return (
-      <MySlider marks={marks} name='рейтинг' ariaLabel="рейтинг"/>
-   );
+      <Box ml={10} mr={10}>
+         <Typography mb={10} ml={-10}>Год</Typography>
+         <Slider
+            value={[selectedRating[0],selectedRating[1]]}
+            onChange={handleChange}
+            marks={marks}
+            valueLabelDisplay="auto"
+            getAriaLabel = {() => "рейтинг"}
+            getAriaValueText={()=>`${selectedRating} рейтинг`}
+            min={marks[0].value}
+            max={marks[1].value}
+         />
+      </Box>
+   )
 }
 
 export default RatingSlider
--- src/components/YearsSlider.jsx
+++ src/components/YearsSlider.jsx
@@ -1,20 +1,30 @@
-import MySlider from "./MySlider"
-
-const marks = [
-   {
-      value: 1920,
-      label: "1920"
-   },
-   {
-      value: 2021,
-      label: "2021"
-   }
-];
+import {Box, Slider, Typography} from "@material-ui/core"
+import {useDispatch, useSelector} from "react-redux"
+import {selectYears} from "../toolkitRedux/toolkitSlice"
 
 function YearsSlider() {
+   const {selectedYears, marks} = useSelector(state => state.toolkit.years)
+   const dispatch = useDispatch()
+
+   const handleChange = (event, newValue) => {
+      dispatch(selectYears(newValue))
+   }
+
    return (
-      <MySlider marks={marks} name='год' ariaLabel="диапазон лет"/>
-   );
+      <Box ml={10} mr={10}>
+         <Typography mb={10} ml={-10}>Год</Typography>
+         <Slider
+            value={[selectedYears[0],selectedYears[1]]}
+            onChange={handleChange}
+            marks={marks}
+            valueLabelDisplay="auto"
+            getAriaLabel = {() => "диапазон лет"}
+            getAriaValueText={()=>`${selectedYears} год`}
+            min={marks[0].value}
+            max={marks[1].value}
+         />
+      </Box>
+   )
 }
 
 export default YearsSlider
