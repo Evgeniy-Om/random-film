eecd5ed345022d932e3bf7fd8f700889ab4a2612
Evgeniy Om <evgenicuss@gmail.com>
2021-07-13 15:42:27 +0300

    заменен useState на Redux Toolkit

--- src/components/CountrySelect.jsx
+++ src/components/CountrySelect.jsx
@@ -2,34 +2,32 @@ import InputLabel from '@material-ui/core/InputLabel'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
-import {useState} from "react";
-import {Box} from "@material-ui/core";
-
-const countriesList = [
-   {id: 0, country: "Все страны"},
-   {id: 2, country: "Россия", addId: 13},
-   {id: 1, country: "США"},
-   {id: 25, country: "Австралия"},
-   {id: 11, country: "Великобритания", addId: 38},
-   {id: 3, country: "Германия", addId: 18},
-   {id: 4, country: "Дания"},
-   {id: 15, country: "Испания"},
-   {id: 14, country: "Италия"},
-   {id: 6, country: "Канада"},
-   {id: 31, country: "Китай", addId: 28},
-   {id: 26, country: "Корея Южная"},
-   {id: 17, country: "Мексика"},
-   {id: 8, country: "Франция"},
-   {id: 5, country: "Швеция"},
-   {id: 9, country: "Япония"},
-]
+import {Box} from "@material-ui/core"
+import {useDispatch, useSelector} from "react-redux"
+import {selectCountry} from "../toolkitRedux/toolkitSlice"
 
 function CountrySelect() {
-   const [country, setCountry] = useState('Все страны')
+   const countriesList = [
+      {id: 0, country: "Все страны"},
+      {id: 2, country: "Россия", addId: 13},
+      {id: 1, country: "США"},
+      {id: 25, country: "Австралия"},
+      {id: 11, country: "Великобритания", addId: 38},
+      {id: 3, country: "Германия", addId: 18},
+      {id: 4, country: "Дания"},
+      {id: 15, country: "Испания"},
+      {id: 14, country: "Италия"},
+      {id: 6, country: "Канада"},
+      {id: 31, country: "Китай", addId: 28},
+      {id: 26, country: "Корея Южная"},
+      {id: 17, country: "Мексика"},
+      {id: 8, country: "Франция"},
+      {id: 5, country: "Швеция"},
+      {id: 9, country: "Япония"},
+   ]
 
-   const handleChange = (event) => {
-      setCountry(event.target.value)
-   };
+   const country = useSelector(state => state.toolkit.country)
+   const dispatch = useDispatch()
 
    return (
       <Box mt={10}>
@@ -40,7 +38,7 @@ function CountrySelect() {
                id="country-select"
                value={country}
                label="Country"
-               onChange={handleChange}
+               onChange={e => dispatch(selectCountry(e.target.value))}
             >
                {countriesList.map((item) => (
                   <MenuItem
