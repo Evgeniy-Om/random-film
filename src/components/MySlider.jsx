8c5cca429a5c7d5547ceb38d857f750e90971f45
Evgeniy Om <evgenicuss@gmail.com>
2021-07-13 15:41:02 +0300

    заменён атрибут слайдера arial-label на getAriaLabel (Material UI ругался)

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -12,11 +12,11 @@ function MySlider({marks, name, ariaLabel}) {
       <Box ml={10} mr={10}>
          <Typography mb={10} ml={-10}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
          <Slider
-            aria-label={ariaLabel}
             value={value}
             onChange={handleChange}
             marks={marks}
             valueLabelDisplay="auto"
+            getAriaLabel = {()=> ariaLabel}
             getAriaValueText={()=>`${value} год`}
             min={marks[0].value}
             max={marks[1].value}
