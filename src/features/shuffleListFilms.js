0e03f0092ef8302742667f34e969d9a7787ff343
Evgeniy Om <evgenicuss@gmail.com>
2021-07-22 23:53:12 +0300

    Добавлены комментарии в функцию случайной сортировки массива

---
+++ src/features/shuffleListFilms.js
@@ -0,0 +1,13 @@
+
+// Тасование Фишера — Йетса
+// https://learn.javascript.ru/task/shuffle
+
+function shuffleListFilms(array) {
+   for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+   }
+   return array
+}
+
+export default shuffleListFilms
