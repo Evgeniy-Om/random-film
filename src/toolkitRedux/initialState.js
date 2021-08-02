3f7608366d6c82cbd213ce1376760ec25774da79
Evgeniy Om <evgenicuss@gmail.com>
2021-08-02 09:23:39 +0300

    рефакторинг

--- src/toolkitRedux/initialState.js
+++ src/toolkitRedux/initialState.js
@@ -1,15 +1,12 @@
-const thisYear = (new Date()).getFullYear()
+import top250ListFilms from "../features/top250ListFilms";
 
 const initialState = {
    selectedCountry: {id: 0, country: "Все страны"},
    selectedGenre: {id: 0, genre: "Все жанры"},
-   selectedYears: [1920, thisYear],
+   selectedYears: [1920, (new Date()).getFullYear()],
    selectedRating: [0, 10],
-   filmsAPI: {
-      shuffleListFilms: {},
-      currentFilmNumber: 0
-   }
-
+   currentFilmNumber: 0,
+   shuffledListFilms: top250ListFilms
 }
 
 export default initialState
