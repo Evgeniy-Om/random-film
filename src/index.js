09d878320276d140324188b8abd72d258cd9a5f1
Evgeniy Om <evgenicuss@gmail.com>
2021-09-02 08:46:29 +0300

    remove React.StrictMode

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ import {store} from "./toolkitRedux";
 import {Provider} from "react-redux";
 
 
-
 ReactDOM.render(
-      <React.StrictMode>
-         <Provider store={store}>
-         <App />
-         </Provider>
-      </React.StrictMode>
-  ,
-  document.getElementById('root')
+   <Provider store={store}>
+      <App/>
+   </Provider>
+   ,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your toolkitRedux, pass a function
