52e8b686d09e9cc930e30b4c8233b28b64bce908
Evgeniy Om <evgenicuss@gmail.com>
2021-09-02 08:48:06 +0300

    добавление асинхронного запроса с параметрами фильтров

--- src/toolkitRedux/toolkitSlice.js
+++ src/toolkitRedux/toolkitSlice.js
@@ -1,7 +1,39 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
 import initialState from "./initialState"
 import shuffleListFilms from "../features/shuffleListFilms";
+import axios from "axios";
 
+export const fetchFilms = createAsyncThunk(
+   'toolkit/fetchFilms',
+   async function (_, {getState}) {
+      const {
+         selectedCountry,
+         selectedGenre,
+         selectedRating,
+         selectedYears,
+         currentFilmNumber
+      } = getState().toolkit
+
+      const fetchParams = {
+         ratingFrom: selectedRating[0],
+         ratingTo: selectedRating[1],
+         yearFrom: selectedYears[0],
+         yearTo: selectedYears[1],
+         page: currentFilmNumber
+      }
+      if (selectedCountry.id !== 0) fetchParams.country = selectedCountry.id
+      if (selectedGenre.id !== 0) fetchParams.genre = selectedGenre.id
+      const response = await axios({
+         url: `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-filters`,
+         params: fetchParams,
+         headers: {
+            'X-API-KEY': 'b7f13992-d5e9-4deb-a225-1692bcdd1f07'
+            // 9cf1fa82-1cb3-4ab0-b073-f34c936caf95
+         }
+      })
+      return response.data
+   }
+)
 const toolkitSlice = createSlice({
       name: 'toolkit',
       initialState: initialState,
@@ -24,10 +56,14 @@ const toolkitSlice = createSlice({
          shuffle(state) {
             state.shuffledListFilms = shuffleListFilms(state.shuffledListFilms)
          }
+      },
+      extraReducers: {
+         [fetchFilms.pending]: () => {},
+         [fetchFilms.fulfilled]: () => {},
+         [fetchFilms.rejected]: () => {}
       }
    }
 )
-
 
 
 export default toolkitSlice.reducer
@@ -38,4 +74,5 @@ export const {
    selectYears,
    selectRating,
    shuffle,
-   numFilm} = toolkitSlice.actions
+   numFilm
+} = toolkitSlice.actions
