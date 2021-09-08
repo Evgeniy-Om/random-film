9cb925a1a011384c416b83212937674c9a72f2d9
Evgeniy Om <evgenicuss@gmail.com>
2021-09-08 20:29:18 +0300

    удаление ненужного

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals'
 import {store} from "./toolkitRedux";
 import {Provider} from "react-redux";
 
@@ -13,9 +11,4 @@ ReactDOM.render(
    </Provider>
    ,
    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your toolkitRedux, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
