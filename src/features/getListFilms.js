e3fa86207f80dfb4ee302e8a5869bb190c39897d
Evgeniy Om <evgenicuss@gmail.com>
2021-09-08 16:37:22 +0300

    blank

--- src/features/getListFilms.js
+++ src/features/getListFilms.js
@@ -1,17 +1,20 @@
-import axios from "axios";
+import axios from "axios"
+import shuffleListFilms from "./shuffleListFilms";
 
-function getListFilms() {
+function getListFilms(countryId, genreId, years, rating) {
    axios({
-      url: 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-filters?country=1&genre=2&genre=0&order=RATING&type=FILM&ratingFrom=5&ratingTo=10&yearFrom=1998&yearTo=2019&page=5',
+      url: `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-filters?country=${countryId}&genre=${genreId}&order=RATING&ratingFrom=1&ratingTo=10&yearFrom=1920&yearTo=2021&page=1`,
       headers: {
          'X-API-KEY': 'b7f13992-d5e9-4deb-a225-1692bcdd1f07'
       }
    })
       .then(function (response) {
          // handle success
-         console.log(response.data);
+         console.log(response.data.films)
+         const arr = [...response.data.films]
+         console.log(shuffleListFilms(arr));
+         return arr
       })
-
 }
 
 export default getListFilms
