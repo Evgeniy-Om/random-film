ffd6fa12077d5bf805ecec9239973f8fcbb8e6a0
Evgeniy Om <evgenicuss@gmail.com>
2021-09-08 16:31:10 +0300

    добавление асинхронных функций (запрос фильмов при изменении фильтров)

--- src/toolkitRedux/toolkitSlice.js
+++ src/toolkitRedux/toolkitSlice.js
@@ -5,13 +5,13 @@ import axios from "axios";
 
 export const fetchFilms = createAsyncThunk(
    'toolkit/fetchFilms',
-   async function (_, {getState}) {
+   async function (_, {dispatch, getState, rejectWithValue}) {
       const {
          selectedCountry,
          selectedGenre,
          selectedRating,
          selectedYears,
-         currentFilmNumber
+         totalPagesResponse
       } = getState().toolkit
 
       const fetchParams = {
@@ -19,7 +19,54 @@ export const fetchFilms = createAsyncThunk(
          ratingTo: selectedRating[1],
          yearFrom: selectedYears[0],
          yearTo: selectedYears[1],
-         page: currentFilmNumber
+         order: "NUM_VOTE",
+         page: totalPagesResponse
+      }
+      if (selectedCountry.id !== 0) fetchParams.country = selectedCountry.id
+      if (selectedGenre.id !== 0) fetchParams.genre = selectedGenre.id
+      try {
+         const response = await axios({
+            url: `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-filters`,
+            params: fetchParams,
+            headers: {
+               'X-API-KEY': 'b7f13992-d5e9-4deb-a225-1692bcdd1f07'
+               // 9cf1fa82-1cb3-4ab0-b073-f34c936caf95
+            }
+         })
+
+         let responseOK = response && response.status === 200
+
+         if (!responseOK) {
+            debugger
+            throw new Error('Server Error!')
+         }
+         dispatch(isNewFilters(false))
+         return response.data
+      } catch
+         (error) {
+         return rejectWithValue(error.message)
+      }
+   }
+)
+
+export const addFilmsToList = createAsyncThunk(
+   'toolkit/fetchFilms',
+   async function (_, {dispatch, getState}) {
+      const {
+         selectedCountry,
+         selectedGenre,
+         selectedRating,
+         selectedYears,
+         currentPageResponse
+      } = getState().toolkit
+
+      const fetchParams = {
+         ratingFrom: selectedRating[0],
+         ratingTo: selectedRating[1],
+         yearFrom: selectedYears[0],
+         yearTo: selectedYears[1],
+         order: "NUM_VOTE",
+         page: currentPageResponse
       }
       if (selectedCountry.id !== 0) fetchParams.country = selectedCountry.id
       if (selectedGenre.id !== 0) fetchParams.genre = selectedGenre.id
@@ -31,9 +78,13 @@ export const fetchFilms = createAsyncThunk(
             // 9cf1fa82-1cb3-4ab0-b073-f34c936caf95
          }
       })
+      dispatch(isNewFilters(false))
+      dispatch(addFilms(response.data.films))
+
       return response.data
    }
 )
+
 const toolkitSlice = createSlice({
       name: 'toolkit',
       initialState: initialState,
@@ -54,13 +105,45 @@ const toolkitSlice = createSlice({
             state.currentFilmNumber += 1
          },
          shuffle(state) {
-            state.shuffledListFilms = shuffleListFilms(state.shuffledListFilms)
+            state.listFilms = shuffleListFilms(state.listFilms)
+         },
+         isNewFilters(state, action) {
+            state.isChangedFilters = action.payload
+            console.log(state.isChangedFilters)
+         },
+         numPagesResponse(state, action) {
+
+         },
+         changeNumPageResponse(state) {
+            (state.isChangedFilters)
+               ? state.currentPageResponse = 1
+               : state.currentPageResponse += 1
+         },
+         addFilms(state, action) {
+            state.listFilms = state.listFilms.concat(action.payload)
+         },
+         disableButton(state, action) {
+            state.isDisabledRandomFilmButton = action.payload
+            console.log(`Disabled: ${state.isDisabledRandomFilmButton}`)
          }
       },
       extraReducers: {
-         [fetchFilms.pending]: () => {},
-         [fetchFilms.fulfilled]: () => {},
-         [fetchFilms.rejected]: () => {}
+         [fetchFilms.pending]: (state) => {
+            state.status = 'loading'
+            state.error = null
+         },
+         [fetchFilms.fulfilled]: (state, action) => {
+            state.status = 'resolved'
+            state.listFilms = action.payload.films
+            state.totalPagesResponse = action.payload.pagesCount
+            console.log(action)
+            console.log(state.totalPagesResponse)
+
+         },
+         [fetchFilms.rejected]: (state, action) => {
+            state.status = 'rejected'
+            state.error = action.payload
+         }
       }
    }
 )
@@ -74,5 +157,9 @@ export const {
    selectYears,
    selectRating,
    shuffle,
-   numFilm
+   numFilm,
+   isNewFilters,
+   changeNumPageResponse,
+   addFilms,
+   disableButton
 } = toolkitSlice.actions
