e0cc39f69e167b33b651537a84b1af45e002ad11
Evgeniy Om <evgenicuss@gmail.com>
2021-09-08 16:36:22 +0300

    добавление новых переменных в стейт

--- src/toolkitRedux/initialState.js
+++ src/toolkitRedux/initialState.js
@@ -1,12 +1,19 @@
-import top250ListFilms from "../features/top250ListFilms";
+import top250ListFilms from "./top250ListFilms";
 
 const initialState = {
    selectedCountry: {id: 0, country: "Все страны"},
    selectedGenre: {id: 0, genre: "Все жанры"},
    selectedYears: [1920, (new Date()).getFullYear()],
    selectedRating: [0, 10],
-   currentFilmNumber: 0,
-   shuffledListFilms: top250ListFilms
+   currentFilmNumber: 1,
+   listFilms: top250ListFilms,
+   isChangedFilters: false,
+   currentPageResponse: 1,
+   totalPagesResponse: 1,
+   totalFilteredFilms: 0,
+   isDisabledRandomFilmButton: false,
+   status: null,
+   error: null,
 }
 
 export default initialState
