4738f054df6eb4a1305ea9929e0fec61d9837b35
Evgeniy Om <evgenicuss@gmail.com>
2021-09-08 16:35:20 +0300

    рефакторинг имён переменных

--- src/components/NameFilm.jsx
+++ src/components/NameFilm.jsx
@@ -2,17 +2,16 @@ import {Box, Skeleton, Typography} from "@material-ui/core"
 import {useSelector} from "react-redux"
 
 function NameFilm() {
-   const {currentFilmNumber, shuffledListFilms} = useSelector(state => state.toolkit)
-   const name = shuffledListFilms[currentFilmNumber].nameRu
-   const year = shuffledListFilms[currentFilmNumber].year
+   const {currentFilmNumber, listFilms} = useSelector(state => state.toolkit)
+   const {nameRu, year, rating} = listFilms[currentFilmNumber]
    return (
       <Box sx={{margin: '20px 0 30px'}}>
-         {name
+         {nameRu
             ? <Typography sx={{
                fontSize: '25px',
                textAlign: 'center'
             }}>
-               {name} ({year})
+               {nameRu} ({year}). Рейтинг: {rating}
             </Typography>
             : <Skeleton variant='text' animation="wave" width={200} height={38}/>}
       </Box>
